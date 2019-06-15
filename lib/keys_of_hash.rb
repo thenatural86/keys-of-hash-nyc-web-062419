15595fab0f8d97e23ccf6345f47d771ca97ae144
The Natural <philip.smalls86@gmail.com>
2019-06-15 16:49:23 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,8 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     array = []
-    arguments.each do |key
+    arguments.each do |key|
+      self.each do ||
        if arguments == key 
          return val 
        end
